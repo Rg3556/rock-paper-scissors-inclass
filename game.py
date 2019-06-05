265f7b5bddebe0523690aab0d78cef455a28cfd4
Rg3556 <50929687+Rg3556@users.noreply.github.com>
2019-06-05 19:46:26 -0400

    Determining winner options

--- game.py
+++ game.py
@@ -30,4 +30,28 @@ print("COMPUTER CHOICE: ", computer_choice)
 
 # Determine the winner
 
+# rock beats scissors
+# paper beats rock
+# scissors beats paper
+# Same selections is a tie
+
+# Plan before starting coding
+if user_choice == computer_choice:
+    print("TIE")
+elif user_choice == "rock" and computer_choice == "paper":
+    print("PAPER")
+elif user_choice == "rock" and computer_choice == "scissors":
+    print("ROCK")        
+
+elif user_choice == "paper" and computer_choice == "rock":
+    print("PAPER")
+elif user_choice == "paper" and computer_choice == "scissors":
+    print("SCISSORS")
+
+elif user_choice == "scissors" and computer_choice == "paper":
+    print("SCISSORS")
+elif user_choice == "scissors" and computer_choice == "rock":
+    print("ROCK")        
+
+
 # Display final outputs / outcomes
