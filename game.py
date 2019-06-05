d3435b252e699e9c02f9dcb1782fc6d3eec2a144
Rg3556 <50929687+Rg3556@users.noreply.github.com>
2019-06-05 19:30:50 -0400

    simplifying valid functions

--- game.py
+++ game.py
@@ -15,14 +15,15 @@ print("USER CHOICE: ", user_choice)
 
 # Validate inputs
 
-if user_choice not in ["rock", "paper", "scissors"]:
+options = ["rock", "paper", "scissors"]
+if user_choice not in options:
     print("INVALID SELECTION, PLEASE TRY AGAIN...")
     exit()
 
 
 # Generate computer selection
 
-computer_choice = random.choice(["rock", "paper", "scissors"])
+computer_choice = random.choice(options)
 print("-------------------------------")
 print("GENERATING...")
 print("COMPUTER CHOICE: ", computer_choice)
